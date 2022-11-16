bc459b7f0bc5024a85c2ed903615ef7a45fd1a31
NSaiEPS <sainizameps@gmail.com>
2022-11-16 10:56:14 +0530

    Splash screen

--- App.js
+++ App.js
@@ -1,96 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+
+  Image,
   StyleSheet,
   Text,
-  useColorScheme,
+
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title})=> {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+
 
 const App= () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  let [splashScreen,setsplashScreen]=useState(true)
+  useEffect(()=>{
+setTimeout(() => {
+  setsplashScreen(false)
+  
+}, 2000);
+  },[])
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+if(splashScreen){
+  return(
+    <View 
+    style={{
+      justifyContent:'center',
+      flex:1,
+      // backgroundColor:"green"
+    }}
+    >
+      <View style={{
+        display:'flex',
+        justifyContent:'center',
+        width:'100%',
+        height:'100%',
+        // backgroundColor:'red',
+        alignSelf:'center',
+        
+      }}>
+    <Image 
+  source={require('./Constants/YouTube_SPlashScreeen..jpg')}
+  // source={require('./Constants/splash.jpg')}
+    style={{
+      width:'100%',
+      height:'100%',
+      // width:'85%',
+      //     height:'85%',
+        
+        // borderRadius:150,
+        resizeMode: 'contain'
+        
+        
+        
+    }}
+    />
+    </View>
+    </View>)
+}
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>Thank you Jesus for this..</Text>
+    </View>
   );
 };
 
